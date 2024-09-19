23bc8c933a19acd3a7e3fd4118ff47c7a3eed719
Mary Sharp <marysharp0721@gmail.com>
2024-09-19 14:27:09 +0100

    Added the rest of the client files

---
+++ client/app.js
@@ -0,0 +1,14 @@
+console.log("Hello");
+
+//!we need a DOM manipulation
+//select the fom and the feedback container
+
+
+//!FORM
+//we need an evenet to submit the form later
+//event handler and event listener
+
+//prevent the default behaviour
+//we need 
+
+//event handler --> submit
